c5ee4373e201e813b944927c6df762dc532117e1
Claudio B Sanches <claudiobuzzolini@gmail.com>
2022-02-09 15:47:40 -0300

    Implantação do model - M do MVC

---
+++ src/main/java/com/AppRH/AppRH/models/Empresa.java
@@ -0,0 +1,106 @@
+package com.AppRH.AppRH.models;
+
+import java.io.Serializable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+public class Empresa implements Serializable {
+	
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long codigo;
+	
+	@NotEmpty
+	private String nome;
+	
+	@Column(unique = true)
+	private String cnpj;
+	
+	private String email;
+	private String telefone;
+	private String responsavel;
+	
+	@OneToMany(mappedBy = "empresa", cascade = CascadeType.REMOVE)
+	private List<Vaga> vaga;
+	
+	@OneToMany(mappedBy = "empresa", cascade = CascadeType.REMOVE)
+	private List<Funcionario>funcionarios;
+
+	public long getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(long codigo) {
+		this.codigo = codigo;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCnpj() {
+		return cnpj;
+	}
+
+	public void setCnpj(String cnpj) {
+		this.cnpj = cnpj;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public String getResponsavel() {
+		return responsavel;
+	}
+
+	public void setResponsavel(String responsavel) {
+		this.responsavel = responsavel;
+	}
+
+	public List<Vaga> getVaga() {
+		return vaga;
+	}
+
+	public void setVaga(List<Vaga> vaga) {
+		this.vaga = vaga;
+	}
+
+	public List<Funcionario> getFuncionarios() {
+		return funcionarios;
+	}
+
+	public void setFuncionarios(List<Funcionario> funcionarios) {
+		this.funcionarios = funcionarios;
+	}
+
+
+
+}
